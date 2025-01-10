1604f16023d21f34bab7b1bae3b2261c0bd0a4cc
Pedro Bosch <pedro@andared.com>
2025-01-10 10:16:08 +0100

    card_swiper

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -12,16 +12,24 @@ class CardSwiper extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: size.height * 0.5,
-      color: Colors.red,
+      //color: Colors.red,
+      padding: const EdgeInsets.all(30),
       child: Swiper(
         itemCount: 10,
         layout: SwiperLayout.STACK,
         itemWidth: size.width*0.6,
         itemHeight: size.height*0.9,
         itemBuilder: (context, index) {
-          return FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'), 
-            image: NetworkImage('https://i.pinimg.com/originals/f4/d7/24/f4d72491c0366c61b1d24912bc0a8079.png')
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://i.pinimg.com/originals/f4/d7/24/f4d72491c0366c61b1d24912bc0a8079.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           );
         },
 
