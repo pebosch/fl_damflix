5510fb7560546975797cac500fa19420a9500777
Pedro Bosch <pedro@andared.com>
2025-01-13 12:39:50 +0100

    FlexibleSpaceBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,9 +6,38 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      floating: true, 
+      pinned: true, 
+      snap: false,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          color: Colors.black54,
+          alignment: Alignment.bottomCenter,
+          child: Text('pelicula.titulo', style: TextStyle(color: Colors.white),)
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://es.web.img2.acsta.net/medias/nmedia/18/35/41/59/18831270.jpg'),
+          fit: BoxFit.cover,
         ),
       ),
     );
