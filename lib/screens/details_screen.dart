2d6570199ee2310777274390a155eacc8f0dfa36
Pedro Bosch <pedro@andared.com>
2025-01-17 08:50:48 +0100

    _InfoPelicula

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -11,7 +11,8 @@ class DetailsScreen extends StatelessWidget {
           _CustomAppBar(),
           SliverList(
             delegate: SliverChildListDelegate([
-              Text('Prueba numero 1'),
+              //Text('Prueba numero 1'),
+              _InfoPelicula()
             ]),
           )
         ],
@@ -44,6 +45,47 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://es.web.img2.acsta.net/medias/nmedia/18/35/41/59/18831270.jpg'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _InfoPelicula extends StatelessWidget {
+  const _InfoPelicula({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://www.starwarsnewsnet.com/wp-content/uploads/2016/07/Starlog-02.jpg'),
+              height: 250,
+            ),
+          ),
+
+          SizedBox( width: 20,),
+
+          Column(
+            children: [
+              Text('pelicula.titulo'),
+              Text('pelicula.year'),
+              Text('pelicula.director'),
+              
+              Row(
+                children: [
+                  Icon(Icons.star, size: 25, color: Colors.yellow),
+                  Text('pelicula.valoracion')
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
