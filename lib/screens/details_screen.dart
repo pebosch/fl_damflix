f5659c9fecf1283b974ab24cf809b80963a9dc9d
Pedro Bosch <pedro@andared.com>
2026-01-09 08:48:12 +0100

    details_screen (Hasta descripción)

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -12,7 +12,8 @@ class DetailsScreen extends StatelessWidget {
           SliverList(
             delegate: SliverChildListDelegate([
               //Text('Probandoooooo')
-              _InfoPelicula()
+              _InfoPelicula(),
+              _Overview(),
             ])
           )
         ],
@@ -71,14 +72,41 @@ class _InfoPelicula extends StatelessWidget {
             ),
           ),
 
+          SizedBox( width: 20,),
+
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Vamos por aquí')
+              Text('movie.title', style: Theme.of(context).textTheme.headlineMedium, overflow: TextOverflow.ellipsis, maxLines: 2,),
+              Text('movie.year', style: Theme.of(context).textTheme.headlineSmall, overflow: TextOverflow.ellipsis, maxLines: 1,),
+
+              Row(
+                children: [
+                  Icon(Icons.star_half, size: 30, color: Colors.orange,),
+                  Text('movie.rate', style: Theme.of(context).textTheme.headlineSmall, overflow: TextOverflow.ellipsis, maxLines: 1,),
+                ],
+                )
+
             ],
           )
         ],
       ),
 
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'movie.description. Id excepteur consequat nisi irure excepteur amet proident dolor nisi quis sunt est voluptate. Officia id ad reprehenderit culpa deserunt. Consequat culpa anim sint id irure laborum anim. Nostrud sit deserunt culpa ex aute reprehenderit qui laboris excepteur quis occaecat adipisicing.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
