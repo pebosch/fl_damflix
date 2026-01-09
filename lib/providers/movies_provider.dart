8076a4b3a92add9d7b5431a55cba042ae67b27b7
Pedro Bosch <pedro@andared.com>
2026-01-09 10:18:14 +0100

    movies_provider actualizado.
    He cambiado la linea 24 del fichero movies_provider a get(url)

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+String _apiKey = 'f13f15ad46ff8743695b6f4ab37a82c7';
+String _baseUrl = 'api.themoviedb.org';
+String _languaje = 'es-ES';
+
   MoviesProvider() {
     print('MoviesProvider ha sido inicializado');
 
@@ -9,6 +15,14 @@ class MoviesProvider extends ChangeNotifier{
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _languaje,
+      'page': '1'
+    }   
+    );
+    var response = await http.get(url);
+
+    print( response.body );
   }
 }
