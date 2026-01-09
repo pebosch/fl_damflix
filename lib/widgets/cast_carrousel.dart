a59e6d59fccb40f9a7d024d7aa579c8d68ee49d9
Pedro Bosch <pedro@andared.com>
2026-01-09 09:05:10 +0100

    CastCarrousel finalizado

--- lib/widgets/cast_carrousel.dart
+++ lib/widgets/cast_carrousel.dart
@@ -7,8 +7,8 @@ class CastCarrousel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 100,
-      color: Colors.yellow,
+      height: 150,
+      //color: Colors.yellow,
       child: ListView.builder(
         itemCount: 10,
         scrollDirection: Axis.horizontal,
@@ -28,8 +28,31 @@ class _CastCard extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric( horizontal: 5),
       width: 80,
-      height: 100,
-      color: Colors.blue,
+      height: 120,
+      //color: Colors.blue,
+      child: Column(
+        children: [
+          //Imagen del actor
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://hips.hearstapps.com/hmg-prod/images/harrison-ford-joven-658b217b0e4eb.jpg'),
+              height: 100,
+              width: 80,
+              fit: BoxFit.cover
+            ),
+          ),
+
+          Text(
+            'actor.name Harrison Ford',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            )
+          //Nombre
+        ],
+      ),
     );
   }
 }
